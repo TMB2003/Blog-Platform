3fa4fdd6cae6c6799df799f0104621f565eba1e0
Taha <taha@Tahas-MacBook-Air.local>
2026-06-04 12:04:14 +0530

    Feat: Initializing the Project

---
+++ src/test/java/com/learning/blogPlatform/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.blogPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
